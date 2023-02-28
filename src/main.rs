248d5dcb099a5edcc5aaca034b74eda509158684
Carlos Aguilera <18aguilerac@gmail.com>
2023-02-27 19:42:43 -0800

    ok this is super fast pace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use rusty_audio::Audio;
+use std::{error::Error, io, time::Duration};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "audio/explode.wav");
+    audio.add("lose", "audio/lose.wav");
+    audio.add("move", "audio/move.wav");
+    audio.add("pew", "audio/pew.wav");
+    audio.add("startup", "audio/startup.wav");
+    audio.add("win", "audio/win.wav");
+
+    audio.play("startup");
+
+    // terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    // clean up;
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    return Ok(());
 }
